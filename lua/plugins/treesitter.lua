2d639339dd93b1e775f9b2336d71c53624cc8cce
Enzo Juhel <enzo.juhel@epita.fr>
2024-11-23 14:39:24 +0100

    Update

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,57 +1,28 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
-
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
+-- nvim-treesitter for enhanced syntax highlighting
+return{
+	{
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { 
+	"c",
+	"bash",
         "html",
-        "css",
-        "prisma",
+        "javascript",
+        "json",
+        "lua",
         "markdown",
         "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
+        "python",
         "query",
-        "vimdoc",
-        "c",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
-}
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml", },
+        highlight = { enable = true }
+      })
+    end
+  },
+  }
